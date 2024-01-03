b0c8a91065f0f2542fc7459d412fdaab250faecf
crrenatacr <crrenatacr@gmail.com>
2024-01-03 14:58:26 +0100

    pokemonList identation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,65 @@
-let pokemonList= [{name:'pikachu', height:'0.4m', type:'electric', abilities:['static', 'lightningrod']}, 
-                 {name:'vulpix', height:'0.6m', type:'fire', abilities:['flash-fire', 'drought']},
-                 {name:'zubat', height:'0.8m', type: ['poison', 'flying'], abilities: ['inner-focus, infiltrator']},
-                 {name:'pidgeot', height:'1.5m', type:['flying', 'normal'], abilities:['keen-eye, tangled-feet, big-pecks']},
-                 {name:'wigglytuff', height:'1m', type:['fairy', 'normal'], abilities:['cute-charm, frisk']},
-                 {name:'flareon', height:'0.9m', type:'fire', abilities:['flash-fire', 'guts']},
-                 {name:'bellossom', height:'0.4m', type:'grass', abilities:['chlorophyll', 'healer']},
-                 {name:'vileplume', height:'1.2m', type:['grass', 'poison'], abilities:['effect-spore', 'chlorophyll']},
-                 {name:'horsea', height:'0.4m', type:'water', abilities: ['damp', 'swift-swim', 'sniper']}]
+let pokemonList = [
+    {
+        name: 'pikachu',
+        height: '0.4m',
+        type: 'electric',
+        abilities: ['static', 'lightningrod']
+    },
+
+    {
+        name: 'vulpix',
+        height: '0.6m',
+        type: 'fire',
+        abilities: ['flash-fire', 'drought']
+    },
+
+    {
+        name: 'zubat',
+        height: '0.8m',
+        type: ['poison', 'flying'],
+        abilities: ['inner-focus, infiltrator']
+    },
+
+    {
+        name: 'pidgeot',
+        height: '1.5m',
+        type: ['flying', 'normal'],
+        abilities: ['keen-eye, tangled-feet, big-pecks']
+    },
+
+    {
+        name: 'wigglytuff',
+        height: '1m',
+        type: ['fairy', 'normal'],
+        abilities: ['cute-charm, frisk']
+    },
+
+    {
+        name: 'flareon',
+        height: '0.9m',
+        type: 'fire',
+        abilities: ['flash-fire', 'guts']
+    },
+
+    {
+        name: 'bellossom',
+        height: '0.4m',
+        type: 'grass',
+        abilities: ['chlorophyll', 'healer']
+    },
+
+    {
+        name: 'vileplume',
+        height: '1.2m',
+        type: ['grass', 'poison'],
+        abilities: ['effect-spore', 'chlorophyll']
+    },
+
+    {
+        name: 'horsea',
+        height: '0.4m',
+        type: 'water',
+        abilities: ['damp', 'swift-swim', 'sniper']
+    }];
 console.log(pokemonList);
 
