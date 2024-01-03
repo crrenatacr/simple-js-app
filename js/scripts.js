32c00b0d8c0911ef1c2c1b1ec39473229ed2ebd5
crrenatacr <crrenatacr@gmail.com>
2024-01-03 14:28:48 +0100

    Add var pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = 'icecream';
-document.write(favoriteFood);
+let pokemonList= [{name:'pikachu', height:'0.4m', type:'electric', abilities:['static', 'lightningrod']}, 
+                 {name:'vulpix', height:'0.6m', type:'fire', abilities:['flash-fire', 'drought']},
+                 {name:'zubat', height:'0.8m', type: ['poison', 'flying'], abilities: ['inner-focus, infiltrator']},
+                 {name:'pidgeot', height:'1.5m', type:['flying', 'normal'], abilities:['keen-eye, tangled-feet, big-pecks']},
+                 {name:'wigglytuff', height:'1m', type:['fairy', 'normal'], abilities:['cute-charm, frisk']},
+                 
+]
 
