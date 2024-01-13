8483fa414ae7804e44d52e4cb07448f6e849b678
crrenatacr <crrenatacr@gmail.com>
2024-01-13 18:58:52 +0100

    Loop Fix trial

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
 let pokemonList = [
     {
         name: 'Pikachu',
-        height: '0.4',
+        height: 0.4,
         type: 'electric',
         abilities: ['static', 'lightningrod']
     },
 
     {
         name: 'Vulpix',
-        height: '0.6',
+        height: 0.6,
         type: 'fire',
         abilities: ['flash-fire', 'drought']
     },
 
     {
         name: 'Zubat',
-        height: '0.8',
+        height: 0.8,
         type: ['poison', 'flying'],
         abilities: ['inner-focus, infiltrator']
     },
 
     {
         name: 'Pidgeot',
-        height: '1.5',
+        height: 1.5,
         type: ['flying', 'normal'],
         abilities: ['keen-eye, tangled-feet, big-pecks']
     },
 
     {
         name: 'Wigglytuff',
-        height: '1',
+        height: 1,
         type: ['fairy', 'normal'],
         abilities: ['cute-charm, frisk']
     },
 
     {
         name: 'Flareon',
-        height: '0.9',
+        height: 0.9,
         type: 'fire',
         abilities: ['flash-fire', 'guts']
     },
 
     {
         name: 'Bellossom',
-        height: '0.4',
+        height: 0.4,
         type: 'grass',
         abilities: ['chlorophyll', 'healer']
     },
 
     {
         name: 'Vileplume',
-        height: '1.2m',
+        height: 1.2,
         type: ['grass', 'poison'],
         abilities: ['effect-spore', 'chlorophyll']
     },
 
     {
         name: 'Horsea',
-        height: '0.4m',
+        height: 0.4,
         type: 'water',
         abilities: ['damp', 'swift-swim', 'sniper']
     }];
@@ -65,7 +65,7 @@ let pokemonList = [
 
     for (let i = 0; i < pokemonList.length; i++) {
         let comment = "";
-        if (pokemonList[i].height > 0.4 && pokemonList[i].height < 0.8) {
+        if (pokemonList[i].height = 0.4 && pokemonList[i].height <= 0.8) {
             comment = " - It's a small Pokemon";
         } else if (pokemonList[i].height >= 0.9 && pokemonList[i].height <= 1.2) {
             comment = " - It's an average Pokemon";
