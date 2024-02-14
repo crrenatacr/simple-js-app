bc6973951805f0a7a8fb6e2a97be81f4c00b836a
crrenatacr <crrenatacr@gmail.com>
2024-02-14 15:20:56 +0100

    Exercise1.6followingExerciseVideo

--- js/scripts.js
+++ js/scripts.js
@@ -1,106 +1,105 @@
-let pokemonRepository = (function() {
-  let pokemonList = (function() {
-    return [
-      {
-        name: 'Pikachu',
-        height: 0.4,
-        type: 'electric',
-        abilities: ['static', 'lightningrod']
-      },
+var pokemonRepository = (function () {
+  let repository = [
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      type: 'electric',
+      abilities: ['static', 'lightningrod']
+    },
 
-      {
-        name: 'Vulpix',
-        height: 0.6,
-        type: 'fire',
-        abilities: ['flash-fire', 'drought']
-      },
+    {
+      name: 'Vulpix',
+      height: 0.6,
+      type: 'fire',
+      abilities: ['flash-fire', 'drought']
+    },
 
-      {
-        name: 'Zubat',
-        height: 0.8,
-        type: ['poison', 'flying'],
-        abilities: ['inner-focus', 'infiltrator']
-      },
+    {
+      name: 'Zubat',
+      height: 0.8,
+      type: ['poison', 'flying'],
+      abilities: ['inner-focus', 'infiltrator']
+    },
 
-      {
-        name: 'Pidgeot',
-        height: 1.5,
-        type: ['flying', 'normal'],
-        abilities: ['keen-eye', 'tangled-feet', 'big-pecks']
-      },
+    {
+      name: 'Pidgeot',
+      height: 1.5,
+      type: ['flying', 'normal'],
+      abilities: ['keen-eye', 'tangled-feet', 'big-pecks']
+    },
 
-      {
-        name: 'Wigglytuff',
-        height: 1,
-        type: ['fairy', 'normal'],
-        abilities: ['cute-charm', 'frisk']
-      },
+    {
+      name: 'Wigglytuff',
+      height: 1,
+      type: ['fairy', 'normal'],
+      abilities: ['cute-charm', 'frisk']
+    },
 
-      {
-        name: 'Flareon',
-        height: 0.9,
-        type: 'fire',
-        abilities: ['flash-fire', 'guts']
-      },
+    {
+      name: 'Flareon',
+      height: 0.9,
+      type: 'fire',
+      abilities: ['flash-fire', 'guts']
+    },
 
-      {
-        name: 'Bellossom',
-        height: 0.4,
-        type: 'grass',
-        abilities: ['chlorophyll', 'healer']
-      },
+    {
+      name: 'Bellossom',
+      height: 0.4,
+      type: 'grass',
+      abilities: ['chlorophyll', 'healer']
+    },
 
-      {
-        name: 'Vileplume',
-        height: 1.2,
-        type: ['grass', 'poison'],
-        abilities: ['effect-spore', 'chlorophyll']
-      },
+    {
+      name: 'Vileplume',
+      height: 1.2,
+      type: ['grass', 'poison'],
+      abilities: ['effect-spore', 'chlorophyll']
+    },
 
-      {
-        name: 'Horsea',
-        height: 0.4,
-        type: 'water',
-        abilities: ['damp', 'swift-swim', 'sniper']
-      }
-    ];
-  })();
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  function add(item) {
-    if (typeof item === 'object' && Object.keys(item).length === 4) {
-      pokemonList.push(item);
-    } else {
-      console.error('Invalid Pokémon data.');
+    {
+      name: 'Horsea',
+      height: 0.4,
+      type: 'water',
+      abilities: ['damp', 'swift-swim', 'sniper']
     }
-  }
-
-  function findByName(name) {
-    return pokemonList.filter(function(pokemon) {
-      return pokemon.name.toLowerCase() === name.toLowerCase();
-    });
-  }
-
-  return {
-    getAll: getAll,
-    add: add,
-    findByName: findByName
-  };
+  ];
 })();
 
+function add(pokemon) {
+  if (
+    typeof pokemon === 'object' &&
+    'name' in pokemon &&
+    'height' in pokemon &&
+    'type' in pokemon
+  ) {
+    repository.push(pokemon);
+  } else {
+    console.log('Invalid Pokemon data.');
+  }
+}
+
+function getAll() {
+  return repository;
+}
+return {
+  add: add,
+  getAll: getAll, 
+  };
+
+
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: "Pikachu", height: 0.3,
-type: ["electric"] });
+pokemonRepository.add({ name: 'Pikachu', height: 0.3,
+type: ['electric'], abilities: ['static', 'lightningrod'] });
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach (function (pokemon) {
-  let.pokemonList = document.querySelector(".pokemon-list");
-  let listpokemon = document.createElement("li");
-  let button = document.createElement("button");
-  button.innerText = "placeholder";
-  button.classList.add("button=class")
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let.pokemonList = document.querySelector('.pokemon-list');
+  let listpokemon = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add('button-class');
+  listpokemon.appendChild(button);
+  pokemonList.appendChild(listpokemon);
+
 });
